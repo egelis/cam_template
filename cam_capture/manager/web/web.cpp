0f589fceda18baaacd78091819d8fd51137f4cf9
Overseven <specregister@gmail.com>
2021-05-22 14:20:47 +0300

    async frame reading proposal

--- cam_capture/manager/web/web.cpp
+++ cam_capture/manager/web/web.cpp
@@ -21,24 +21,47 @@ std::vector<Metadata> WebCamManager::getCamsList() {
 // Проходить в цикле каждую камеру (занося ее в новый поток) и вызывать frames раз getNewFrame
 
 Cams WebCamManager::getFrames(size_t frames) {
-    std::vector<std::future<Cam>> futures;
-    for (const auto &cam: m_cams) {
-        futures.push_back(std::async([&] {
-                              Frames res(frames);
-                              for (size_t frame = 0; frame < frames; frame++) {
-                                  res[frame] = cam->getNewFrame();
-                              }
-                              return Cam{cam->meta.dev_path, std::move(res)};
-                          })
-        );
+//    std::vector<std::future<Cam>> futures;
+
+//    for (const auto &cam: m_cams) {
+//        futures.push_back(std::async([&] {
+//                              Frames res(frames);
+//                              for (size_t frame = 0; frame < frames; frame++) {
+//                                  res[frame] = cam->getNewFrame();
+//                              }
+//                              return Cam{cam->meta.dev_path, std::move(res)};
+//                          })
+//        );
+//    }
+
+//    size_t num_of_cameras = m_cams.size();
+//    Cams res(num_of_cameras);
+
+//    for (size_t cam = 0; cam < num_of_cameras; cam++) {
+//        res[cam] = futures[cam].get();
+//    }
+
+
+    Cams res(m_cams.size());
+    // сразу выделяем нужный объём памяти
+    for (size_t c = 0; c < res.size(); c++) {
+        res.resize(frames);
+        // res[c].first = ?; // можно в Metadata камеры добавить поле name, и тут его считывать
+    }
+    // для каждого кадра
+    for (size_t frame = 0; frame < frames; frame++) {
+        // создаём вектор фьючеров (1 фьючер - 1 камера)
+        std::vector<std::future<Frame>> futures;
+        // запускаем потоки приёма кадров со всех камер
+        for (size_t c = 0; c < m_cams.size(); c++) {
+             futures.push_back(std::async(&ICamera::getNewFrame, m_cams[c]));
+        }
+        // ожидаем завершения
+        for (size_t c = 0; c < m_cams.size(); c++) {
+            res[c].second[frame] = futures[c].get();
+        }
     }
 
-    size_t num_of_cameras = m_cams.size();
-    Cams res(num_of_cameras);
-
-    for (size_t cam = 0; cam < num_of_cameras; cam++) {
-        res[cam] = futures[cam].get();
-    }
 
     return res;
 }
